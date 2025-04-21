7b1ba2c3a78ede80b2f238d13ca2bfc38efc2929
Guilherme Henrique <guihenrique17020@uin9.edu.br>
2025-04-20 23:43:43 -0300

    Correção de Erro no backend

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); 
 
+const app = express();
 
-app.use = express.json();
 const PORT = process.env.PORT || 5500;
 
 app.use(cors());
-app.use = (express.json());
+app.use(express.json());
 app.use(bodyParser.json());
+
 app.get('/',async (req, res) =>{
   res.send('O Servidor está online Plenamente');
 });
